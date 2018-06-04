66a6aa5f453465c7a0f0860291841b8c9ca4ed6d
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-06-04 14:37:02 +0100

    add connect to the app container with mapDispatchToProps and mapStateToProps

---
+++ src/redux/appReducer/appReducer.js
@@ -0,0 +1,24 @@
+export const CHANGE_TEXT = 'app/CHANGE_TEXT'
+
+export const changeText = (text) => {
+  return {
+    type: CHANGE_TEXT,
+    payload: text
+  }
+}
+
+const reducer = (state = {}, action = {}) => {
+  switch(action.type) {
+    case CHANGE_TEXT:
+      const { text } = action.payload
+
+      return {
+        ...state,
+        text
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
---
+++ src/redux/appReducer/index.js
@@ -0,0 +1,1 @@
+export { default, changeText } from './appReducer'
